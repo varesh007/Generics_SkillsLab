afded19866f4d2d113432adb4d657bbdfafadc1a
Poomun, Faraz <Faraz.Poomun@Ceridian.com>
2021-09-16 20:18:34 +0400

    Before reflection

--- Interns.cs
+++ Interns.cs
@@ -3,8 +3,10 @@
     public class Jedi : IInterns
     {
         private int Id;
-        private string Name;
+        public string Name;
+
         public string NameId => $"Jedi - {Name}: {Id}";
+        public int Age { get; set; }
 
         public Jedi(int id, string name)
         {
@@ -16,8 +18,12 @@
     public class SkillsLab : IInterns
     {
         private int Id;
-        private string Name;
+        public string Name;
+
         public string NameId => $"SkillsLab - {Name}: {Id}";
+        public int Age { get; set; }
+
+        public int EmployeeId { get; set; }
 
         public SkillsLab(int id, string name)
         {
